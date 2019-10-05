acdd7a2ff5503f6abd865c561dcea921bf2152e5
github username <github email address>
2019-10-05 12:29:45 -0500

    EAGIOJHUN#####

--- app/graphql/types/breeder_type.rb
+++ app/graphql/types/breeder_type.rb
@@ -5,7 +5,8 @@ module Types
   
       field :id, ID, null: false
   
-      field :date_of_birth, String, null: false
+      field :address, String, null: false
       field :first_name, String, null: false
       field :last_name, String, null: false
+      field :authorized_on, String, null: false
     end
